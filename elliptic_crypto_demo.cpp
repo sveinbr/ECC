a14b8e0aedf43f25bb671fe51d30509a1d338844
Svein Brekke <sveinbr@gmail.com>
2018-11-06 20:45:48 +0100

    Removed the struct "integer", define the actual integer type used as template parameter.

--- elliptic_crypto_demo.cpp
+++ elliptic_crypto_demo.cpp
@@ -1,23 +1,15 @@
-struct integer // Define as a separate type to spot automatic type conversions.
-{
-    typedef int value_type;
-    value_type i;
-    integer operator%(value_type p) const noexcept { return integer{ (i%p) + (i < 0 ? p : 0) }; } // Always 0 <= result < p
-    integer operator*(integer other) const noexcept { return integer{ i*other.i }; }
-    //integer operator+(integer other) const noexcept { return integer{ i + other.i }; }
-    //integer operator-() const noexcept { return integer{ -i }; }
-};
-constexpr integer operator"" _i(unsigned long long i) { return integer{ static_cast<integer::value_type>(i) }; }
+template<typename Int>
+Int modulus(Int i, Int p) noexcept { return (i%p) + (i < 0 ? p : 0); } // Always 0 <= modulus(i,p) < p, thus not the same as operator% for negative values
 
-template <integer::value_type p>
-integer InvertModuloP(integer x) noexcept
+template<typename Int, Int p>
+Int InvertModuloP(Int x) noexcept
 {
-    auto ExtendedEuclidianAlgorithm = [](integer::value_type a, integer::value_type b)
+    auto ExtendedEuclidianAlgorithm = [](Int a, Int b)
     {
-        struct { integer::value_type r, s, t; } new_{ b,0,1 }, old_{ a,1,0 };
+        struct { Int r, s, t; } new_{ b,0,1 }, old_{ a,1,0 };
         while (new_.r != 0)
         {
-            integer::value_type quotient = old_.r / new_.r;
+            Int quotient = old_.r / new_.r;
             decltype(old_) temp_ = old_;
             old_ = new_;
             new_ = { temp_.r - quotient*old_.r, temp_.s - quotient*old_.s, temp_.t - quotient*old_.t }; // new = temp - quotient*old
@@ -25,48 +17,47 @@ integer InvertModuloP(integer x) noexcept
         return old_;
     };
 
-    auto divisor = ExtendedEuclidianAlgorithm(x.i % p, p);
-    return integer{ divisor.r == 1 ? divisor.s : 0 } % p;
+    auto divisor = ExtendedEuclidianAlgorithm( modulus(x, p), p);
+    return divisor.r == 1 ? modulus(divisor.s, p) : 0;
 }
 
 
-
-template <integer::value_type p>
+template<typename Int, Int p>
 struct FP_element
 {
-    integer value;
-    bool operator==(FP_element other) const noexcept { return value.i == other.value.i; }
-    FP_element operator-() const noexcept { return{ integer{ value.i==0 ? 0 : p - value.i } }; } // Optimized "(-value)%p"
-    FP_element operator+(FP_element other) const noexcept  { return FP_element{ value.i + other.value.i >= p ? value.i + other.value.i - p : value.i + other.value.i }; }  // Optimized "(value+other.value)%p"
-    FP_element operator-(FP_element other) const noexcept { return FP_element{ value.i >= other.value.i ? value.i - other.value.i : p - (other.value.i - value.i)}; } // Optimized "(value-other.value)%p"
-    FP_element operator*(FP_element other) const noexcept { return FP_element{ (other.value*value) % p }; }
-    FP_element operator/(FP_element divisor) const noexcept { return FP_element{ (value * InvertModuloP<p>(divisor.value)) % p }; }
-    friend FP_element<p> operator*(integer k, FP_element<p> x) noexcept { return FP_element<p>{ (k*x.value) % p }; }
+    Int value;
+    bool operator==(FP_element other) const noexcept { return value == other.value; }
+	FP_element operator-() const noexcept { return FP_element{ value == 0 ? 0 : p - value }; } // Optimized "modulus(-value,p)"
+    FP_element operator+(FP_element other) const noexcept  { return FP_element{ value + other.value >= p ? value + other.value - p : value + other.value }; }  // Optimized "modulus(value+other.value, p)"
+    FP_element operator-(FP_element other) const noexcept { return FP_element{ value >= other.value ? value - other.value : p - (other.value - value)}; } // Optimized "modulus(value-other.value, p)"
+    FP_element operator*(FP_element other) const noexcept { return FP_element{ modulus(other.value * value, p) }; }
+    FP_element operator/(FP_element divisor) const noexcept {  return FP_element{ modulus(value * InvertModuloP<Int, p>(divisor.value), p) };}
+    friend FP_element<Int,p> operator*(Int k, FP_element<Int,p> x) noexcept { return FP_element<Int,p>{ modulus(k*x.value, p) }; }
 };
 
 
 
-template <integer::value_type p, integer::value_type a, integer::value_type b>
+template <typename Int, Int p, Int a, Int b>
 struct Point
 {
-    FP_element<p> x, y;
+    FP_element<Int, p> x, y;
     Point operator-() const noexcept { return Point{ x, -y }; }
     Point operator-(Point other) const noexcept { return *this + (-other); }
     bool operator==(Point other) const noexcept { return x == other.x && y == other.y; }
-    friend Point<p, a, b> operator+(Point<p, a, b> P, Point<p, a, b> Q) noexcept
+    friend Point<Int, p, a, b> operator+(Point<Int, p, a, b> P, Point<Int, p, a, b> Q) noexcept
     {
-        const Point<p, a, b> ZERO{ 0,0 };
+        const Point<Int, p, a, b> ZERO{ 0,0 };
         if (P == ZERO) return Q;
         if (Q == ZERO) return P;
         if (P.y == -Q.y) return{ 0,0 };
-        FP_element<p> m = P == Q ?
-            (integer{ 3 } *P.x * P.x + FP_element<p>{a}) / (integer{ 2 } *P.y)
+        FP_element<Int, p> m = P == Q ?
+            (Int{ 3 } * P.x * P.x + FP_element<Int, p>{a}) / (Int{ 2 } * P.y)
             : (P.y - Q.y) / (P.x - Q.x);
-        FP_element<p> x = m * m - P.x - Q.x;
-        FP_element<p> y = P.y + m*(x - P.x);
+        FP_element<Int, p> x = m * m - P.x - Q.x;
+        FP_element<Int, p> y = P.y + m*(x - P.x);
         return{ x, -y };
     }
-    friend Point<p, a, b> operator*(integer k, Point<p, a, b> P) noexcept
+    friend Point<Int, p, a, b> operator*(Int k, Point<Int, p, a, b> P) noexcept
     {
         Point Q = P;
         while (--k.value.i > 0) // O(k) for simplicity, can be improved to O(log(k))
@@ -76,29 +67,25 @@ struct Point
 };
 
 // Defining a specific curve of the class y^2 = x^3 + ax + b in the space F_p
-template <integer::value_type FieldOrder, integer::value_type A, integer::value_type B>
+template <typename Int, Int FieldOrder, Int A, Int B>
 struct EllipticCurve
 {
-    static const integer::value_type a = A, b = B, p = FieldOrder;
-    typedef ::Point<p, a, b> Point;
+    static const Int a = A, b = B, p = FieldOrder;
+    typedef ::Point<Int, p, a, b> Point;
+    typedef FP_element<Int, p> Element;
 
     bool contains(Point P) const noexcept
     {
-		auto y_2 = P.y*P.y;
-		auto x_3 = P.x*P.x*P.x;
-		auto ax = FP_element<p>{ a }*P.x;
-		auto b_ = FP_element<p>{ b };
-		auto rhs = x_3 + ax + b_;
-        return P.y*P.y == P.x*P.x*P.x + FP_element<p>{ a }*P.x + FP_element<p>{ b };
+        return P.y*P.y == P.x*P.x*P.x + a*P.x + Element{ b };
     }
 
-    integer::value_type order(Point P) const noexcept
+    Int order(Point P) const noexcept
     {
         if (!contains(P))
             return 0;
         const Point ZERO{ 0,0 };
         Point nP = P;
-        integer::value_type n{ 1 };
+        Int n{ 1 };
         while (!(nP == ZERO))
         {
             ++n;
@@ -107,11 +94,11 @@ struct EllipticCurve
         return n;
     }
 
-    integer::value_type order() const noexcept // brute force. Consider Schoof's algorithm.
+    Int order() const noexcept // brute force. Consider Schoof's algorithm.
     {
-        integer::value_type out{ 1 }; // Include infinity
-        for (integer::value_type x = 0; x < p; ++x)
-            for (integer::value_type y = 0; y < p; ++y)
+        Int out{ 1 }; // Include infinity
+        for (Int x = 0; x < p; ++x)
+            for (Int y = 0; y < p; ++y)
                 if (contains(Point{ x,y }))
                     out++;
         return out;
@@ -120,16 +107,16 @@ struct EllipticCurve
 
 
 #include <iostream>
-template <integer::value_type p> void Require_that_mutiplication_and_division_modulo_p_are_consistent()
+template <typename Int, Int p> void Require_that_mutiplication_and_division_modulo_p_are_consistent()
 {
     int errors = 0;
-    for (integer::value_type x = 1; x < p; x++)
+    for (Int x = 1; x < p; x++)
     {
-        for (integer::value_type y = 1; y < p; y++)
+        for (Int y = 1; y < p; y++)
         {
-            FP_element<p> X{ x }, Y{ y };
+            FP_element<Int,p> X{ x }, Y{ y };
             auto X2 = (X*Y) / Y, X3 = (X / Y) * Y;
-            if (X.value.i != X2.value.i || X.value.i != X3.value.i)
+            if (X.value != X2.value || X.value != X3.value)
             {
                 std::cout << "FAIL: " << __FUNCTION__ << ", X=" << x << ", Y=" << y << std::endl;
                 errors++;
@@ -141,19 +128,19 @@ template <integer::value_type p> void Require_that_mutiplication_and_division_mo
 }
 
 
-void Require_that_EC_7_6_3_has_order_7_and_base_point_3_0_with_order_7()
+template <typename Int> void Require_that_EC_7_6_3_has_order_7_and_base_point_3_0_with_order_7()
 {
-    typedef EllipticCurve<7, 6, 3> EC;
+    typedef EllipticCurve<Int, 7, 6, 3> EC;
     EC C;
-	if (C.order() == 6)
-		std::cout << "OK:   " << __FUNCTION__ << std::endl;
-	else
-		std::cout << "FAIL: " << __FUNCTION__ << "EC [p=" << C.p << ", a=" << C.a << ", b=" << C.b << "] has order " << C.order() << " (expected 6)" << std::endl;
+    if (C.order() == 6)
+        std::cout << "OK:   " << __FUNCTION__ << std::endl;
+    else
+        std::cout << "FAIL: " << __FUNCTION__ << "EC [p=" << C.p << ", a=" << C.a << ", b=" << C.b << "] has order " << C.order() << " (expected 6)" << std::endl;
 
-	for (integer::value_type y = EC::p - 1; y >= 0; --y)
+    for (Int y = EC::p - 1; y >= 0; --y)
     {
-		for (integer::value_type x = 0; x < EC::p; ++x)
-		{
+        for (Int x = 0; x < EC::p; ++x)
+        {
             if (C.contains({ x,y }))
                 std::cout << C.order({ x,y }) << "   ";
             else
@@ -165,7 +152,9 @@ void Require_that_EC_7_6_3_has_order_7_and_base_point_3_0_with_order_7()
 
 int main()
 {
-    Require_that_mutiplication_and_division_modulo_p_are_consistent<7>();
-    Require_that_EC_7_6_3_has_order_7_and_base_point_3_0_with_order_7();
+    Require_that_mutiplication_and_division_modulo_p_are_consistent<int,7>();
+    Require_that_EC_7_6_3_has_order_7_and_base_point_3_0_with_order_7<int>();
+    Require_that_mutiplication_and_division_modulo_p_are_consistent<long, 7>();
+    Require_that_EC_7_6_3_has_order_7_and_base_point_3_0_with_order_7<long>();
     return 0;
 }
